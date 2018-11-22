84bad6562b159579da4b0f5c6528f5d3564370a4
Bence Mándli <mandli.bence@gmail.com>
2018-11-22 16:32:01 +0100

    Improve header deck size selector

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,30 +2,62 @@ import React from 'react';
 import logo from '../svgs/supercharge-logo.svg';
 import DeckSelector from "./DeckSelector";
 
-const Header = props => (
-    <header>
-        <nav className="navbar navbar-dark bg-dark navbar-expand-md">
-             <span className="navbar-brand">
-                <img src={logo} width="30" height="30"
-                     className="d-inline-block align-top logo"
-                     alt="logo"/>
-                Memory game
-             </span>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler"
-                    aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"/>
-            </button>
-            {props.isGameStart &&
-            <div className="collapse navbar-collapse" id="navbarToggler">
-                <ul className="navbar-nav mr-auto mt-2 mt-lg-2">
-                    <li className="nav-item active">
-                        <DeckSelector type="header-deck-size" startGame={props.startGame}/>
-                    </li>
-                </ul>
-            </div>
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isMenuActive: false
+        }
+    }
+
+    toggleMenu = () => {
+        const isMenuActive = !this.state.isMenuActive;
+        this.setState({isMenuActive});
+    };
+
+    renderDeckSelector = () => {
+        if (this.props.isGameStart) {
+            let className = 'navbar-collapse';
+            if (!this.state.isMenuActive) {
+                className += ' collapse';
             }
-        </nav>
-    </header>
-);
+
+            return (
+                <React.Fragment>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarToggler"
+                            aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation"
+                            onClick={this.toggleMenu}>
+                        <span className="navbar-toggler-icon"/>
+                    </button>
+                    <div className={className} id="navbarToggler">
+                        <ul className="navbar-nav mr-auto mt-2 mt-lg-2">
+                            <li className="nav-item active">
+                                <DeckSelector type="header-deck-size" startGame={this.props.startGame}/>
+                            </li>
+                        </ul>
+                    </div>
+                </React.Fragment>
+            )
+        }
+    };
+
+    render() {
+        return (
+            <header>
+                <nav className="navbar navbar-dark bg-dark navbar-expand-md">
+                    <span className="navbar-brand">
+                        <img src={logo} width="30" height="30"
+                             className="d-inline-block align-top logo"
+                             alt="logo"/>
+                        Memory game
+                    </span>
+                    {this.renderDeckSelector()}
+                </nav>
+            </header>
+        )
+    }
+}
 
 export default Header;
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Scoreboard = props => (
     <div className="scoreboard container">
         <div className="row">
-            <span className="col-sm-4 align-middle"><span className="current-tries">Current tries:</span> {props.currentScore}</span>
-            <span className="col-sm-4 align-middle text-center best-score">Best <br/><span className="best-value">{props.highScore}</span></span>
-            <div className="col-sm-4 align-middle text-right">
+            <span className="col-4 align-middle"><span className="current-tries">Current tries:</span> {props.currentScore}</span>
+            <span className="col-4 align-middle text-center best-score">Best <br/><span className="best-value">{props.highScore}</span></span>
+            <div className="col-4 align-middle text-right">
                 <button className="btn btn-outline-dark" onClick={() => props.startGame()}>Restart</button>
             </div>
         </div>
